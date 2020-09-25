57b676a8cdc3eeedfc8deb534e56a6e028dc8f3d
ofbennett <ofbennett@gmail.com>
2020-09-25 20:06:05 +0100

    [model_lstm, add]: Adds embedding matrix construction to the training pipeline

--- model_lstm/model_lstm/pipeline.py
+++ model_lstm/model_lstm/pipeline.py
@@ -7,8 +7,13 @@ lstm_pipeline = Pipeline(
     [
         ("text_cleaning", pp.TextCleaning(regex=config.TEXT_CLEANING_REGEX)),
         ("text_stemming", pp.TextStemming(skip=config.SKIP_STEMMING)),
-        ("text_stop_word_removal", pp.TextStopWordRemoval(skip=config.SKIP_STOP_WORD_REMOVAL, stop_words=config.STOP_WORDS)),
-        ("text_encoding", enc.TextEncoding(num_words=config.NUM_WORDS)),
+        ("text_stop_word_removal", pp.TextStopWordRemoval(skip=config.SKIP_STOP_WORD_REMOVAL, 
+                                                            stop_words=config.STOP_WORDS)),
+        ("text_encoding", enc.TextEncoding(glove_embedding_path= (config.DATA_DIR / config.GLOVE_EMBEDDINGS_FILE),
+                                            embedding_matrix_path= (config.DATA_DIR / config.EMBEDDING_MATRIX_FILE),
+                                            num_words=config.NUM_WORDS,
+                                            embedding_dim=config.WORD_EMBEDDINGS_DIM,
+                                            words_kept=config.NUM_WORDS)),
         ("sequence_padding", enc.SequencePadding(max_length=config.PADDING_MAX_LENGTH)),
     ]
 )
--- model_lstm/model_lstm/utils/encoders.py
+++ model_lstm/model_lstm/utils/encoders.py
@@ -1,19 +1,53 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 class TextEncoding(BaseEstimator, TransformerMixin):
-    def __init__(self, num_words):
+    def __init__(self, glove_embedding_path, embedding_matrix_path, num_words, embedding_dim, words_kept):
         self.tokenizer = Tokenizer(num_words=num_words, oov_token="OOV")
+        self.glove_embedding_path = glove_embedding_path
+        self.embedding_matrix_path = embedding_matrix_path
+        self.num_words = num_words
+        self.embedding_dim = embedding_dim
+        self.words_kept = words_kept
 
     def fit(self, X, y = None):
         self.tokenizer.fit_on_texts(X["text"])
+        self.build_embedding_matrix()
         return self
 
     def transform(self, X):
         X = X.copy()
         X_encoded = self.tokenizer.texts_to_sequences(X["text"])
         return X_encoded
+    
+    def build_embedding_matrix(self):
+        embedding_dict = {}
+        with open(self.glove_embedding_path) as f:
+            for line in f:
+                items = line.split()
+                word = items[0]
+                embedding = np.array(items[1:], dtype="float32")
+                embedding_dict[word] = embedding
+        
+        num_tokens = len(self.tokenizer.word_index) + 2
+        hits = 0
+        misses = 0
+
+        embedding_matrix = np.zeros((num_tokens, self.embedding_dim))
+        for word, i in self.tokenizer.word_index.items():
+            embedding_vector = embedding_dict.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+                if i < (self.words_kept+2):
+                    hits += 1
+            else:
+                if i < (self.words_kept+2):
+                    misses += 1
+        print(f"Converted {hits} words ({misses} misses) in words kept")
+        np.save(self.embedding_matrix_path, embedding_matrix)
+
 
 class SequencePadding(BaseEstimator, TransformerMixin):
     def __init__(self, max_length):
@@ -26,3 +60,4 @@ class SequencePadding(BaseEstimator, TransformerMixin):
         X = X.copy()
         X_padded = pad_sequences(X, maxlen=self.max_length)
         return X_padded
+
