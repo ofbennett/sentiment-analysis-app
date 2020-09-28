ad7e0737c648655a35dbac084b83d0e276f9de6d
ofbennett <ofbennett@gmail.com>
2020-09-28 19:38:07 +0100

    [model_lstm, add]: Adds script to publish model package.

---
+++ model_lstm/publish.sh
@@ -0,0 +1,20 @@
+export $(cat .env | xargs)
+
+set -e
+
+warn() {
+    echo "$@" 1>&2
+}
+
+die() {
+    warn "$@"
+    exit 1
+}
+
+python setup.py sdist bdist_wheel || die "Building package failed."
+
+for file in $(ls dist)
+do
+    curl -F package=@dist/${file} https://${FURY_TOKEN}@push.fury.io/${FURY_USERNAME}/ || die "Uploading package failed."
+done
+echo "Package successfully published."
