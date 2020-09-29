8c8e73666b30e83908cee29788bcce8bd9147535
ofbennett <ofbennett@gmail.com>
2020-09-29 11:05:44 +0100

    [model_lstm, update]: Updates publish script.

--- model_lstm/publish.sh
+++ model_lstm/publish.sh
@@ -15,6 +15,7 @@ python setup.py sdist bdist_wheel || die "Building package failed."
 
 for file in $(ls dist)
 do
+    echo "Uploading ${file} to index..."
     curl -F package=@dist/${file} https://${FURY_TOKEN}@push.fury.io/${FURY_USERNAME}/ || die "Uploading package failed."
 done
 echo "Package successfully published."
