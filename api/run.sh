6ed80ba2afb2c53ffb04fe2d4148c828a259748e
ofbennett <ofbennett@gmail.com>
2020-10-03 16:08:04 +0100

    [api, fix]: Revert to original gunicorn command.

--- api/run.sh
+++ api/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec gunicorn --bind 0.0.0.0:5000 --timeout 60 --access-logfile - --error-logfile - run:application
+exec gunicorn --bind 0.0.0.0:5000 --access-logfile - --error-logfile - run:application
